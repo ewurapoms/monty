efc8ac00c6283f666ea64047e9a7e9c324b9b57b
ewurapoms <apowurah@outlook.com>
2023-07-25 17:05:16 -0700

    Project Header File

---
+++ monty.h
@@ -0,0 +1,105 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+typedef void (*instruction_fn)(stack_t **);
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @fn: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	instruction_fn fn;
+} instruction_t;
+
+/**
+ * enum stack_mode_n - stack mode enumeration
+ * @LIFO: operate as a stack
+ * @FIFO: operate as a queue
+ */
+typedef enum stack_mode_n
+{
+	LIFO = 0,
+	FIFO = 1
+} stack_mode_t;
+
+/**
+ * struct op_env_s - operation environment
+ * @sp: top of the stack
+ * @argv: argument vector
+ * @line: line buffer
+ * @linesz: line buffer size
+ * @lineno: line number
+ * @mode: stack operation mode
+ */
+typedef struct op_env_s
+{
+	stack_t *sp;
+	char **argv;
+	char *line;
+	size_t linesz;
+	size_t lineno;
+	stack_mode_t mode;
+} op_env_t;
+
+extern op_env_t op_env;
+
+char **tokenize(char *str);
+
+instruction_fn get_instruction_fn(const char *opcode);
+int isinteger(const char *str);
+
+size_t tokcheck(const char *str);
+
+void errors(const char *fmt, ...);
+void free_op_env(void);
+void free_stack(stack_t **sp);
+void op_add(stack_t **sp);
+void op_div(stack_t **sp);
+void op_mod(stack_t **sp);
+void op_mul(stack_t **sp);
+void op_nop(stack_t **sp);
+void op_pall(stack_t **sp);
+void op_pchar(stack_t **sp);
+void op_pint(stack_t **sp);
+void op_pop(stack_t **sp);
+void op_pstr(stack_t **sp);
+void op_push(stack_t **sp);
+void op_queue(stack_t **sp);
+void op_rotl(stack_t **sp);
+void op_rotr(stack_t **sp);
+void op_stack(stack_t **sp);
+void op_sub(stack_t **sp);
+void op_swap(stack_t **sp);
+
+
+#endif /* MONTY_H */
